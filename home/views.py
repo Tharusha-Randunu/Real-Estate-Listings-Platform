069a0113d6f3339258521c5182518c50677f6e5f
Tharusha-Randunu <tharusharperera@gmail.com>
2025-04-08 12:31:29 +0530

    edited home page

--- home/views.py
+++ home/views.py
@@ -3,5 +3,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse("<h1>Welcome to Real Estate Listings</h1>")
-    return HttpResponse("<h1>Group 5</h1>")
+    return render(request, 'home/home.html')
