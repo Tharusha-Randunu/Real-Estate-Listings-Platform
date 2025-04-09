1a200fbc698f9e738b9f0edadc3f29da952cafb8
Tharusha-Randunu <tharusharperera@gmail.com>
2025-04-09 16:05:13 +0530

    created the find a home page, sqllite database, and the cards

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+
 
 # Register your models here.
+from django.contrib import admin
+from .models import ConfirmedAd, PropertyFeature
+
+admin.site.register(ConfirmedAd)
+admin.site.register(PropertyFeature)
