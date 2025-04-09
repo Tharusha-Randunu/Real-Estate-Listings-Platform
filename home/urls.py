2161533f9f94eaebdf0654a5fc49c4111d76c6d0
Venuja Udukumbura <venuja.udukumbura@gmail.com>
2025-04-09 12:28:18 +0530

    Temp changes to home.html and views.py before switching branches

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import home
+from .views import home, list_property
+
 
 urlpatterns = [
     path('', home, name='home'),
 ]
+
+urlpatterns = [
+    path('', home, name='home'),
+    path('list-property/', list_property, name='list_property'),
+]
+
+
